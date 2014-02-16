796993e518d51073af7fcea1fe94484eee05ba33
Adam L. More <AdamLMore@users.noreply.github.com>
2014-02-16 04:53:47 -0800

    Added some new search parameters

--- TwitterTweetRequester.py
+++ TwitterTweetRequester.py
@@ -6,21 +6,29 @@ consumer_secret     = "your consumer_secret here"
 access_token_key    = "your access_token_key here"
 access_token_secret = "your access_token_secret here"
 
-#This is the search term for which the script returns tweets
-search_term   = "Irvine"
 
-#This is amount of time it takes to refresh the tweets
+#The term that is searched for in the query 
+search_term   = "twitch"
+#The amount of time it takes to refresh the feed
 refresh_delay = 3
+#Latest tweet ID
+latest_id = 0
+#Total number of tweets fetched at a time
+count = 1 
 
 
 ##########################################################################
 # Fetches the Tweets and prints
 
 def requestTweets():
-	r = api.request('search/tweets', {'q':search_term})
+	global latest_id
+	r = api.request('search/tweets', {'q':search_term, 'count':count, 'since_id':int(latest_id)})
 	for item in r.get_iterator():
+		latest_id = item["id"]
 		print item["user"]["screen_name"]
-		print item["text"]+"\n"
+		print item["text"]+"\n\n"
+		print "==========================================================="
+		print "\n"
 	threading.Timer(refresh_delay, requestTweets).start()
 
 ##########################################################################
